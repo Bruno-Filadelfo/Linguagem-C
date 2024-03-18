fe61f180d9285b221e2961ae224b8d0572ce85df
Bruno Carvalho <bruno.filadelfo@outlook.com>
2024-03-18 00:31:44 -0300

    saida de dados em c

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
 
     printf("Ola, mundo!");
     return 0;
-    
+
 }
---
+++ saida_de_dados.c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    
+    printf("Boa noite!m tudo bem com voce ? ");
+    printf("Valor inteiro: %d.\n", 10);
+    printf("Valor real: %f.\n", 8.8);
+    printf("Valor real com aoenas duas casas: %.2f.\n",3.85988);
+    printf("Valor de texto: %c.\n", 'a');
+    printf("Valor de texto: %s.\n", "testando");
+}
+
